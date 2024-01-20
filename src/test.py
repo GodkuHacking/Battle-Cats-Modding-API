612ea7b48297d46a7da62f194654c031596bd6b6
Assasian.Nator <138173273+mindsetpro@users.noreply.github.com>
2024-01-20 10:11:01 -0500

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,22 @@
+import os
+import modder
+
+# Original APK and assets
+original_apk = 'original.apk'
+assets_dir = 'assets'
+os.makedirs(assets_dir, exist_ok=True)
+
+# Create test pack and list
+battlecat_modder.encrypt_files(assets_dir, assets_dir) 
+
+# Test patch file
+with open('new_unit.bin', 'wb') as f:
+  f.write(os.urandom(30))
+
+# Mod APK  
+pack_patch = [('new_unit.bin', 100, 30)]
+modded_apk = 'modded.apk'
+
+battlecat_modder.make_mod_apk(original_apk, assets_dir, pack_patch, modded_apk)
+
+print('Modded APK created!')
